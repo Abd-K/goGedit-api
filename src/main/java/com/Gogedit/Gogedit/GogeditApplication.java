1c6b5c9d8eafcd517df66f8a9ba49c68858d8466
Abdullah Alghali <abdullahalghali31@gmail.com>
2023-07-23 23:53:40 +0100

    initial commit

---
+++ src/main/java/com/Gogedit/Gogedit/GogeditApplication.java
@@ -0,0 +1,13 @@
+package com.Gogedit.Gogedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogeditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogeditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gogedit/Gogedit/GogeditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gogedit.Gogedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogeditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
