a5d5fba0fd243255779e79b505c00091cdb1ffbe
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-08-03 02:04:21 +0100

    fix: remove duplicate from bad merge

--- src/main/java/com/Gogedit/persistence/repository/CommunityRepository.java
+++ src/main/java/com/Gogedit/persistence/repository/CommunityRepository.java
@@ -1,15 +1,13 @@
 package com.Gogedit.persistence.repository;
 
 import com.Gogedit.persistence.entity.Community;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommunityRepository extends JpaRepository<Community, String> {
 
   //TODO costly?
   List<Community> findAllByNameContainingIgnoreCase(String keyword);
-  boolean existsByName(String name);
   boolean existsByName(String name);
   Community findCommunityByName(String name);
 }
